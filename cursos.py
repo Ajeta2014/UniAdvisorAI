7eb7d2e25d4aa72ffa031783db21b8a0b68924b3
Ajeta2014 <ajetaangola@gmail.com>
2026-02-06 20:01:15 +0000

    Add course data structure in cursos.py

---
+++ cursos.py
@@ -0,0 +1,20 @@
+cursos = [
+    {
+        "nome":"Engenharia Mecatrônica",
+        "notas_min":{"matematica":16,"portugues":14,"ciencias":16},
+        "skills":["Robótica","Programação","AI"],
+        "carreira":["Pesquisa","Indústria"]
+    },
+    {
+        "nome":"Medicina",
+        "notas_min":{"matematica":15,"portugues":16,"ciencias":17},
+        "skills":["Biologia","Química","Saúde"],
+        "carreira":["Pesquisa","Clínica"]
+    },
+    {
+        "nome":"Economia",
+        "notas_min":{"matematica":12,"portugues":14,"ciencias":10},
+        "skills":["Matemática","Gestão","Finanças"],
+        "carreira":["Mercado","Finanças"]
+    }
+]
